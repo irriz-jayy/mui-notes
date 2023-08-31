5e367c8c135548b192343fd657339473c96d166c
irriz-jayy <jayfelix789@gmail.com>
2023-08-31 14:26:16 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          <em>Mui practice</em>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
