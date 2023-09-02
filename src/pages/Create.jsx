358fe93759a8d05a63bf3088a554af69216a49e4
irriz-jayy <jayfelix789@gmail.com>
2023-09-02 10:55:34 +0300

    added text fields and states

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,21 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
   return (
     <>
-      <div>
+      <Container>
         <Typography
           variant="h6"
           component="h2"
@@ -13,7 +24,40 @@ const Create = () => {
         >
           Create a new note
         </Typography>
-      </div>
+
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            onChange={(e) => setTitle(e.target.value)}
+            label="Note Title"
+            variant="outlined"
+            fullWidth
+            required
+            margin="normal"
+          />
+          <TextField
+            onChange={(e) => setDetails(e.target.value)}
+            label="Note Details"
+            variant="outlined"
+            fullWidth
+            required
+            multiline
+            rows="4"
+            margin="normal"
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            endIcon={<SendOutlinedIcon />}
+            onClick={() => console.log("You clicked me")}
+          >
+            Submit
+          </Button>
+        </form>
+
+        <br />
+      </Container>
     </>
   );
 };
