bf3cf98afba1f8e57e2ad1ea6e16d7ef52691d6f
irriz-jayy <jayfelix789@gmail.com>
2023-09-06 08:03:21 +0300

    added menu items and links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,33 @@
-import { Box, Drawer, Typography } from "@mui/material";
+import { AddCircleOutline, SubjectOutlined } from "@mui/icons-material";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const drawerWidth = 240;
 const Layout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      text: "My notes",
+      icon: <SubjectOutlined color="seconndary" />,
+      path: "/",
+    },
+    {
+      text: "Create note",
+      icon: <AddCircleOutline color="secondary" />,
+      path: "/create-note",
+    },
+  ];
+
   return (
     <>
       <Box sx={{ display: "flex" }}>
@@ -22,6 +47,19 @@ const Layout = ({ children }) => {
           <div>
             <Typography variant="h5">Jay's Notes</Typography>
           </div>
+
+          <List>
+            {menuItems.map((item) => (
+              <ListItem
+                key={item.text}
+                button
+                onClick={() => navigate(item.path)}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
         </Drawer>
 
         <div>{children}</div>
